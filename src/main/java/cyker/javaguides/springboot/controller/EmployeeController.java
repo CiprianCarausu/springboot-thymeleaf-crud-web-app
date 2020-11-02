fcbb0b92b5a4ddc2d4b6346313758ad8c5f4a202
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-02 15:56:28 +0200

    new_employee.html added

--- src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package cyker.javaguides.springboot.controller;
 
+import cyker.javaguides.springboot.model.Employee;
 import cyker.javaguides.springboot.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +19,13 @@ public class EmployeeController {
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
     }
 
 
--- src/main/java/cyker/javaguides/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/cyker/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -19,4 +19,10 @@ public class EmployeeServiceImpl implements EmployeeService{
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+
+    }
 }
