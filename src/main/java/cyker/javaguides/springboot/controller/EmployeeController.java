41b5aebcbe2b1edb554fce46e64906ee4efdd38a
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-02 15:36:15 +0200

    bootstrap added

--- src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
--- src/main/java/cyker/javaguides/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/cyker/javaguides/springboot/service/EmployeeServiceImpl.java
@@ -2,12 +2,14 @@ package cyker.javaguides.springboot.service;
 
 import cyker.javaguides.springboot.model.Employee;
 import cyker.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
