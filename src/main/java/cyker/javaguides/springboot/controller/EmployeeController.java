c7f0c8dc2a6de0df440e1bc861c99617e073edc9
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-02 16:13:43 +0200

    employee controller/new_employee html - working

--- src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import cyker.javaguides.springboot.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -28,5 +30,10 @@ public class EmployeeController {
         return "new_employee";
     }
 
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
 
+    }
 }
