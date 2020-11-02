e5239f05ddc365eac6abc430d5a6b3787d5eb0fb
Ciprian Carausu <carausuciprian@gmail.com>
2020-11-02 14:30:34 +0200

    controller added

---
+++ src/main/java/cyker/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package cyker.javaguides.springboot.controller;
+
+import cyker.javaguides.springboot.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "index";
+    }
+
+
+}
--- src/main/java/cyker/javaguides/springboot/service/EmployeeService.java
+++ src/main/java/cyker/javaguides/springboot/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package cyker.javaguides.springboot.service;
 
 import cyker.javaguides.springboot.model.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface EmployeeService {
     List<Employee> getAllEmployees();
 }
